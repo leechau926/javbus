3f0b7fe6349ad5dfe25cf47df981b8edfb3792ef
Jack Stanley <leechauisjackmasfather@aliyun.com>
2019-04-02 20:35:02 +0800

    Structure optimized

--- javbus.py
+++ javbus.py
@@ -6,22 +6,22 @@ fanhao_list = ["SSNI-%03d"%x for x in range(1,438)]
 headers = {'User-agent': 'Mozilla/5.0'}
 
 def get(fanhao):
-        url = "https://www.javbus.pw/%s" % fanhao
-        response = requests.get(url)
-        pattern = re.compile("img\s=\s'(.*?)';", re.S)
-        item = re.findall(pattern,response.text)
-        return item[0]
+    url = "https://www.javbus.pw/%s" % fanhao
+    response = requests.get(url)
+    pattern = re.compile("img\s=\s'(.*?)';", re.S)
+    item = re.findall(pattern, response.text)
+    return item[0]
 
 def main():
-        for fanhao in fanhao_list:
-                try:                
-                        filename = "%s.jpg"%fanhao
-                        r = requests.get(get(fanhao), stream=True, headers=headers)
-                        with open(filename, 'wb') as f:
-                                r.raw.decode_content = True
-                                shutil.copyfileobj(r.raw, f)
-                except IndexError:
-                        continue
+    for fanhao in fanhao_list:
+        try:                
+            filename = "%s.jpg" % fanhao
+            r = requests.get(get(fanhao), stream=True, headers=headers)
+            with open(filename, 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+        except IndexError:
+            continue
 
 if __name__ == '__main__':
-        main()
+    main()
