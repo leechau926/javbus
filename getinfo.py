18082b5d449337d7471251ee1cc6d9640243907d
Jack Stanley <leechauisjackmasfather@aliyun.com>
2019-04-08 15:49:59 +0800

    Update getinfo.py

--- getinfo.py
+++ getinfo.py
@@ -3,7 +3,7 @@ import re
 import random
 
 url = "https://www.torrentkitty.tv/search/"
-keyword = "[7sht.me]ipx"
+keyword = input("Please input keyword: ")
 my_headers = ["Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
 "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0",
